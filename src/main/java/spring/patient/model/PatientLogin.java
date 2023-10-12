1c02e5aa0305dbe57131bbffacc1083b678d9859
Gizmo10 <sw.ndzala@gmail.com>
2023-10-12 16:04:55 +0200

    Created model for patientLogin with salt generator

--- src/main/java/spring/patient/model/PatientLogin.java
+++ src/main/java/spring/patient/model/PatientLogin.java
@@ -1,9 +1,13 @@
 package spring.patient.model;
 
+import java.util.Random;
+
 public class PatientLogin {
     private String patientName;
     private String patientSurname;
     private String patientPassword;
+    private String passwordSalt;
+    private String passwordHash;
 
     public PatientLogin() {}
 
@@ -13,24 +17,57 @@ public class PatientLogin {
         this.patientPassword = password;
     }
 
-    public String getPatientName(){
+    public String getPatientName() {
         return patientName; }
 
-    public String getPatientSurname(){
+    public String getPatientSurname() {
         return patientSurname; }
 
-    public String getPatientPassword(){
+    public String getPatientPassword() {
         return patientPassword; }
 
-    public void setPatientName(String name){
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPatientName(String name) {
         this.patientName = name;
     }
 
-    public void setPatientSurname(String surname){
+    public void setPatientSurname(String surname) {
         this.patientSurname = surname;
     }
 
-    public void setPatientPassword(String password){
+    public void setPatientPassword(String password) {
         this.patientPassword = password;
+    }
+
+    public String createPasswordSalt() {
+
+        String alphaNumeric = "ABCDEFGHIJKLNMOPQRSTUVWXYZabcdefghijklmn" +
+                "opqrstuvwxyz0123456789";
+        int alphanumericLength = alphaNumeric.length();
+        StringBuilder sb = new StringBuilder();
+        int saltSize = 20;
+        Random rand = new Random();
+
+        for(int i = 0; i < saltSize; i++) {
+            int index = rand.nextInt(alphanumericLength);
+            char randomCharacter = alphaNumeric.charAt(index);
+            sb.append(randomCharacter);
+        }
+        return sb.toString();
+    }
+
+    public void setPasswordSalt() {
+        passwordSalt = createPasswordSalt();
+    }
+
+    public void setPasswordHash(String password, String salt) {
+
     }
 }
