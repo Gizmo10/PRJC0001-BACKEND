86cc3c34c08329c5ba7bd36094c7a43053192f7f
Gizmo10 <sw.ndzala@gmail.com>
2023-11-30 10:24:11 +0200

    Test PatientLogin class

--- src/test/java/spring/patient/model/PatientLoginTest.java
+++ src/test/java/spring/patient/model/PatientLoginTest.java
@@ -1,9 +1,35 @@
+
 package spring.patient.model;
 
-import org.junit.Test;
+//import org.junit.Test; Tests aren't recognised when you import this
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.*;
+//Make the class public for test to be picked up
+public class PatientLoginTest {
+    private PatientLogin patient = new PatientLogin();
 
-class PatientLoginTest {
+    @Test
+    public void saltCannotBeEmpty(){
+        patient.setPasswordSalt();
+        assertNotNull(patient.getPasswordSalt());
+    }
 
+    @Test
+    public void generatesAnAlphanumericSalt() {
+        String saltPattern = "^[A-Z | a-z | 0-9]([A-Z | a-z | 0-9]{19})$";
+        Pattern pattern = Pattern.compile(saltPattern);
+        patient.setPasswordSalt();
+        Matcher matcher = pattern.matcher(patient.getPasswordSalt());
+        assertTrue(matcher.find());
+    }
+
+    @Test
+    public void theSaltIsAFixedLength() {
+        patient.setPasswordSalt();
+        assertEquals(20, patient.getPasswordSalt().length());
+    }
 }
